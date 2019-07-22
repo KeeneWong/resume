a89f89e02f8f16ced6eae4b98c05dd09bd6097cc
KeeneWong <keenewonghofung@gmail.com>
2019-07-22 14:01:47 -0400

    color

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 #! /usr/bin/env node
 
 const fs = require('fs')
+var colors = require('colors');
 
 fs.readFile(__dirname + '/info.json', 'utf8', function(err, data) {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
-        return data
+        console.log(data.rainbow)
+        return data;
     }
 })
